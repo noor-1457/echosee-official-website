9951adc3578484053bb6f1014dcf718690ecf9ff
hamnashahid231 <hamnashahid231@gmail.com>
2026-02-05 21:12:44 +0500

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import Home from "./Home";
+import Home from "./pages/Home"; 
 import About from "./pages/About";
 import Footer from "./components/Footer";
 import Product from "./pages/Product";
